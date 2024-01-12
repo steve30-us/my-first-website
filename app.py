4836520802b038805d6018ced9070c7b48ddfc3e
Steve Su <test@example.com>
2024-01-12 14:48:59 -0800

    test flash and deleted test.py

---
+++ app.py
@@ -0,0 +1,17 @@
+#print("hello World")
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+#print (__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello World"
+
+if __name__ == '__main__': 
+    # print("I'm inside the if now")
+   app.run(host='0.0.0.0', debug=True)
+
